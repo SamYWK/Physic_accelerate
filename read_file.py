8f209d43c867f05dd72ad58097abdc57e5de648d
scan6 <pig840421@gmail.com>
2018-03-09 15:55:25 +0800

    upload

--- read_file.py
+++ read_file.py
@@ -31,35 +31,41 @@ def _parse_line(line):
 with g_1.as_default():
     
     dataset = tf.data.TextLineDataset(filenames).skip(1)
-    dataset = dataset.apply(tf.contrib.data.map_and_batch(map_func=_parse_line, batch_size=256,\
-                                                          num_parallel_batches= 1))
+    dataset = dataset.map(_parse_line)
+    #dataset = dataset.apply(tf.contrib.data.map_and_batch(map_func=_parse_line, batch_size=256,\
+    #                                                      num_parallel_batches= 1))
+    dataset = dataset.batch(5000)
     dataset = dataset.repeat()
-    dataset = dataset.prefetch(5)
+    dataset = dataset.cache()
+    dataset = dataset.prefetch(1)
     iterator = dataset.make_one_shot_iterator()
     features, label = iterator.get_next()
     
-    a1 = tf.layers.dense(features, 256, tf.nn.relu, name = 'layer_1')
-    a2 = tf.layers.dense(a1, 256, tf.nn.relu, name = 'layer_2')
-    a3 = tf.layers.dense(a2, 256, tf.nn.relu, name = 'layer_3')
-    a4 = tf.layers.dense(a3, 256, tf.nn.relu, name = 'layer_4')
-    a5 = tf.layers.dense(a4, 256, tf.nn.relu, name = 'layer_5')
-    z6 = tf.layers.dense(a5, 1, name = 'layer_6')
     with tf.device('/gpu:0'):
+        a1 = tf.layers.dense(features, 256, tf.nn.relu, name = 'layer_1')
+        a2 = tf.layers.dense(a1, 256, tf.nn.relu, name = 'layer_2')
+        a3 = tf.layers.dense(a2, 256, tf.nn.relu, name = 'layer_3')
+        a4 = tf.layers.dense(a3, 256, tf.nn.relu, name = 'layer_4')
+        a5 = tf.layers.dense(a4, 256, tf.nn.relu, name = 'layer_5')
+        z6 = tf.layers.dense(a5, 1, name = 'layer_6')
+
         loss = tf.losses.mean_squared_error(labels = label, predictions = z6)
         train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
     
-    #prediction
-    #correct_prediction = tf.equal(tf.argmax(x1,1), tf.argmax(y_placeholder,1))
-    #accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    
-    #initializer
-    init = tf.global_variables_initializer()
-    
-    with tf.Session(config = tf.ConfigProto(allow_soft_placement = True, log_device_placement=True)) as sess:
-        sess.run(init)
-        for batch in range(1000):
-            sess.run(train_step)
-            if batch % 10 == 0:
-                print(sess.run(loss))
-        #writer
-        #writer = tf.summary.FileWriter('logs/', sess.graph)
+        #prediction
+        #correct_prediction = tf.equal(tf.argmax(x1,1), tf.argmax(y_placeholder,1))
+        #accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        
+        #initializer
+        init = tf.global_variables_initializer()
+        config = tf.ConfigProto(allow_soft_placement = True, log_device_placement=True)  
+        config.gpu_options.allow_growth = True
+        
+        with tf.Session(config = config) as sess:
+            sess.run(init)
+            for batch in range(1000):
+                sess.run(train_step)
+                if batch % 10 == 0:
+                    print(sess.run(loss))
+            #writer
+            #writer = tf.summary.FileWriter('logs/', sess.graph)
