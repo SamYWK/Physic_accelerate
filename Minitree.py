9d7b0aa2291b0985cab9eef09f23dc5e032651e4
scan6 <pig840421@gmail.com>
2018-04-10 23:56:31 +0800

    upload

--- Minitree.py
+++ Minitree.py
@@ -16,23 +16,23 @@ import sklearn.metrics
 df = pd.read_csv('minitree_4b_2_26.csv', header = 0)
 #y = pd.concat((df['Jet_genjetPt'], df['Target']), axis = 1)
 y = df['Target'].values.reshape(-1, 1)
-X = df.drop(['Target', 'Jet_genjetPt', 'Jet_pt'], axis = 1).values
+X = df.drop(['Target', 'Jet_genjetPt'], axis = 1).values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
 
 print(X_train.shape)
 print(y_train.shape)
 print(X_test.shape)
 
-learning_rate = 0.000001
+learning_rate = 0.00001
 batch_size = 100
-epochs = 5
+epochs = 100
 g_1 = tf.Graph()
 n, d = X_train.shape
 
 def add_layer(inputs, in_dim, out_dim, activation = None, name = 'layer'):
     with tf.name_scope(name):
         with tf.name_scope('Weights'):
-            W = tf.Variable(tf.truncated_normal([in_dim, out_dim]))
+            W = tf.Variable(tf.random_normal([in_dim, out_dim], stddev = 2.0))
         with tf.name_scope('bias'):
             b = tf.Variable(tf.zeros([out_dim]))
         with tf.name_scope('Wx_plus_b'):
@@ -43,45 +43,52 @@ def add_layer(inputs, in_dim, out_dim, activation = None, name = 'layer'):
         return output
 
 with g_1.as_default():
-    with tf.device('/device:GPU:1'):
-        X_placeholder = tf.placeholder(tf.float32, [None, 17])
+    with tf.device('/device:GPU:0'):
+        X_placeholder = tf.placeholder(tf.float32, [None, 18])
         y_placeholder = tf.placeholder(tf.float32, [None, 1])
     
-        a1 = add_layer(X_placeholder, 17, 15, tf.nn.sigmoid, name = 'layer_1')
+        a1 = add_layer(X_placeholder, 18, 15, tf.nn.sigmoid, name = 'layer_1')
         a2 = add_layer(a1, 15, 15, tf.nn.sigmoid, name = 'layer_2')
         a3 = add_layer(a2, 15, 15, tf.nn.sigmoid, name = 'layer_3')
         a4 = add_layer(a3, 15, 15, tf.nn.sigmoid, name = 'layer_4')
-        a5 = add_layer(a4, 15, 10, tf.nn.sigmoid, name = 'layer_5')
-        a6 = add_layer(a5, 10, 10, tf.nn.sigmoid, name = 'layer_6')
-        a7 = add_layer(a6, 10, 10, tf.nn.sigmoid, name = 'layer_7')
-        a8 = add_layer(a7, 10, 5, name = 'layer_8')
-        z9 = add_layer(a8, 5, 1, name = 'layer_9')
+        a5 = add_layer(a4, 15, 15, tf.nn.sigmoid, name = 'layer_5')
+        a6 = add_layer(a5, 15, 15, tf.nn.sigmoid, name = 'layer_6')
+        a7 = add_layer(a6, 15, 15, tf.nn.sigmoid, name = 'layer_7')
+        a8 = add_layer(a7, 15, 15, tf.nn.sigmoid, name = 'layer_8')
+        a9 = add_layer(a8, 15, 15, tf.nn.sigmoid, name = 'layer_9')
+        a10 = add_layer(a9, 15, 13, tf.nn.sigmoid, name = 'layer_10')
+        a11 = add_layer(a10, 13, 13, tf.nn.sigmoid, name = 'layer_11')
+        a12 = add_layer(a11, 13, 10, tf.nn.sigmoid, name = 'layer_12')
+        a13 = add_layer(a12, 10, 10, name = 'layer_13')
+        z14 = add_layer(a13, 10, 1, name = 'layer_14')
     
-        loss = tf.losses.mean_squared_error(labels = y_placeholder, predictions = z9)
+        loss = tf.losses.mean_squared_error(labels = y_placeholder, predictions = z14)
         train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
     #initializer
     init = tf.global_variables_initializer()
 
-    config = tf.ConfigProto(allow_soft_placement = True, log_device_placement=True)
+    config = tf.ConfigProto(allow_soft_placement = False, log_device_placement=True)
     #config.gpu_options.per_process_gpu_memory_fraction = 0.4
     config.gpu_options.allow_growth = True
     
     #saver
     saver = tf.train.Saver()
     with tf.Session(config = config) as sess:
-        sess.run(init)
-        #saver.restore(sess, "./saver/model.ckpt")
+        #sess.run(init)
+        saver.restore(sess, "./saver3/model.ckpt")
         
         for epoch in range(epochs):
             for batch in range(int (n / batch_size)):
                 batch_xs = X_train[(batch*batch_size) : (batch+1)*batch_size]
                 batch_ys = y_train[(batch*batch_size) : (batch+1)*batch_size]
                 sess.run(train_step, feed_dict = {X_placeholder:batch_xs, y_placeholder:batch_ys})
-                if batch % 100000:
+                if batch % 500000:
                     print(sess.run(loss, feed_dict = {X_placeholder:batch_xs, y_placeholder:batch_ys}))
-        saver.save(sess, "./saver/model.ckpt")
+        saver.save(sess, "./saver4/model.ckpt")
         
-        #pred = sess.run(z6, feed_dict = {X_placeholder:X_test})
+        #pred = sess.run(z14, feed_dict = {X_placeholder:X_test})
+        #for i in range(100):
+        #    print(pred[i], y_test[i])
         '''
         Jet_pt_hat = pred * Jet_pt
         error = abs(Jet_pt_hat - predict_target)
@@ -91,5 +98,5 @@ with g_1.as_default():
             if error[i]>0.8 and error[i]<1.6:
                 count+=1
         print(count)
-        
-        print('\nR2_score: ', sklearn.metrics.r2_score(y_true = y_test, y_pred = pred))'''
+        '''
+        #print('\nR2_score: ', sklearn.metrics.r2_score(y_true = y_test, y_pred = pred))
